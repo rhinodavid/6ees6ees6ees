a84f6fe030cc071939cea622cb03fcca56a434b5
David Walsh <dawalsh@gmail.com>
2016-09-19 12:11:46 -0700

    Implement grub

--- src/Grub.js
+++ src/Grub.js
@@ -1,4 +1,14 @@
 class Grub {
+  constructor() {
+    this.age = 0;
+    this.color = 'pink';
+    this.food = 'jelly';
+  }
+  eat() {
+    return 'Mmmmmmmmm jelly';
+  }
 };
+
+
 
 export default Grub;
